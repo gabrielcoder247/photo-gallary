ac738b3e9d635d80bfb9aba1d28bdc7d67d9cf37
gabrielcoder <gabrielcoder247@gmail.com>
2018-09-30 09:26:47 +0300

    create settings for databse and images

--- gallary/settings.py
+++ gallary/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'myimage.apps.MyimageConfig',
+    'bootstrap3',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -75,8 +77,10 @@ WSGI_APPLICATION = 'gallary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'gabrielcoder',
+        'PASSWORD' 'dushanbe2015',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- myimage/models.py
+++ myimage/models.py
@@ -4,11 +4,20 @@ from django.db import models
 
 
 class Image(models.Model):
+    title = models.CharField(max_length = 30)
     name = models.CharField(max_length = 30)
-    description = models.CharField(max_length = 50)
-
+    description = models.TextField(max_length = 150)
+    image_path = models.ImageField(upload_to = 'images/') 
+    location = models.ForeignKey('Location', null= True)
+    category = models.ForeignKey('Category', null= True)
 
 
 class Location(models.Model):
-    
+    name = models.CharField(max_length = 30)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 30)
+
 
