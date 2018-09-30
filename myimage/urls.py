bda133d434313c26238cc77a5f260cc98fc9c447
gabrielcoder <gabrielcoder247@gmail.com>
2018-10-01 01:28:15 +0300

    create category.html template

--- myimage/urls.py
+++ myimage/urls.py
@@ -8,7 +8,10 @@ urlpatterns =[
     url(r'^$',views.index,name='indexPage'),
     # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name='pastNews'),
     # url(r'^article/(\d+)',views.article,name ='article'),
-    url(r'^search/', views.search_results,name='search_results') 
+    url(r'^search/', views.search_results,name='search_results'), 
+    url('locations/<loc>', views.get_image_by_location, name='mylocations'),
+    url(r'categories/<cat>',views.get_image_by_category,name='mycategories'),
+   
 ]
 
 if settings.DEBUG:
